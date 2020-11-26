5c7dbad3d44f251c1d3cd2c39c262d3f18a257de
gabogarciam <gabogarciam71@gmail.com>
2020-11-26 02:23:35 +0100

    Change the logic in navbar component, added svg images and improve the css

---
+++ src/components/Navbar/MenuItems.js
@@ -0,0 +1,22 @@
+export const MenuItems = [
+    {
+        title: 'Home',
+        url: '/',
+        cName: 'nav-links'
+    },
+    {
+        title: 'Exercise List',
+        url: '/all-exercises',
+        cName: 'nav-links'
+    },
+    {
+        title: 'Create Exercise',
+        url: '/create-exercise',
+        cName: 'nav-links'
+    },
+    {
+        title: 'Create User',
+        url: '/create-user',
+        cName: 'nav-links'
+    }
+]
--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,30 +1,40 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
+import { MenuItems } from './MenuItems'
 import './Navbar.css';
 
 class Navbar extends Component {
-    handleClick(e) {
-        const active = document.getElementsByClassName('active');
-        active[0].classList.remove('active');
-        e.target.parentElement.classList.add('active');
+    constructor() {
+        super();
+
+        this.state = {
+            clicked: false
+        }
+    }
+    handleClick = () => {
+        this.setState({
+            clicked: !this.state.clicked
+        })
     }
 
     render() {
         return (
-            <nav className="navbar">
-                <ul className='navbar-nav'>
-                    <li className='navbar-item active' onClick={this.handleClick}>
-                        <Link to='/'>Home</Link>
-                    </li>
-                    <li className='navbar-item' onClick={this.handleClick}>
-                        <Link to='/all-exercises'>Exercise List</Link>
-                    </li>
-                    <li className='navbar-item' onClick={this.handleClick}>
-                        <Link to='/create-exercise'>Create Exercise</Link>
-                    </li>
-                    <li className='navbar-item' onClick={this.handleClick}>
-                        <Link to='/create-user'>Create User</Link>
-                    </li>
+            <nav className="NavbarItems">
+                <Link to='/' className='navbar-logo'>
+                    <div className='image-logo'></div>
+                    <h3 className='nav-title'>ExerciseTracker</h3>
+                </Link>
+                <div className='menu-icon' onClick={this.handleClick}>
+                    <div className={`menu-burger ${this.state.clicked ? 'menu-times' : 'menu-bars'}`}></div>
+                </div>
+                <ul className={this.state.clicked ? 'nav-menu active' : 'nav-menu'}>
+                    {MenuItems.map((item, index) => {
+                        return (
+                            <li className='navbar-item' key={index}>
+                                <Link className={item.cName} to={item.url} onClick={this.handleClick}>{item.title}</Link>
+                            </li>
+                        )
+                    })}
                 </ul>
             </nav>
         )
