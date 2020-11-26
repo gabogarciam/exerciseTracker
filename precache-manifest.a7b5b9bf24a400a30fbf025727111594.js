1f420288ff8b710cf82f6d8f0f1364ce046d4c19
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2020-11-26 01:28:40 +0000

    Deploy gabogarciam/exerciseTracker to github.com/gabogarciam/exerciseTracker.git:gh-pages

---
+++ precache-manifest.a7b5b9bf24a400a30fbf025727111594.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "34bee7eefe35228aa5423ca43bc26480",
+    "url": "/exerciseTracker/index.html"
+  },
+  {
+    "revision": "64a33a466348dc57a89f",
+    "url": "/exerciseTracker/static/css/main.110545ae.chunk.css"
+  },
+  {
+    "revision": "cdca6ec1ff5cdb8cee27",
+    "url": "/exerciseTracker/static/js/2.d8b6c7c8.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/exerciseTracker/static/js/2.d8b6c7c8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "64a33a466348dc57a89f",
+    "url": "/exerciseTracker/static/js/main.8e53e9d5.chunk.js"
+  },
+  {
+    "revision": "2128a39a8f742bfbf6cc",
+    "url": "/exerciseTracker/static/js/runtime-main.3d80e958.js"
+  },
+  {
+    "revision": "2dd8f4f37fa9353d8f385aae252ec6bd",
+    "url": "/exerciseTracker/static/media/logo.2dd8f4f3.svg"
+  },
+  {
+    "revision": "5ed81d50c93529c5aa489a9b45bc02b7",
+    "url": "/exerciseTracker/static/media/menu-bars.5ed81d50.svg"
+  },
+  {
+    "revision": "1626cc8654cad9d098e85bccacb0bec1",
+    "url": "/exerciseTracker/static/media/menu-times.1626cc86.svg"
+  }
+]);
